9a98e1807e678d76c7af2974d1c71becb96cba4a
Daniel Terry <daniel.terry2121@gmail.com>
2021-05-15 16:52:08 -0400

    working chat app with personal styles , starting to add login functionality with local storage

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { ChatEngine } from 'react-chat-engine';
-
+import ChatFeed from './components/ChatFeed';
 import './App.css';
 
 const App = () => {
@@ -9,6 +9,7 @@ const App = () => {
            projectID = "41c02377-56b0-4b93-b5f4-c13869778ff0"
            userName = "Daniel"
            userSecret = "123123"
+           renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} /> }
            />
     )
 }
