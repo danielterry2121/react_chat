f74c520335807697c9c1333d9cf8b0202643a2f7
Daniel Terry <daniel.terry2121@gmail.com>
2021-05-15 17:18:10 -0400

    Finished building out login with local storage

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import { ChatEngine } from 'react-chat-engine';
 import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
 import './App.css';
 
+const projectID = '41c02377-56b0-4b93-b5f4-c13869778ff0';
+
 const App = () => {
-    return (
-        <ChatEngine
-           height="100vh"
-           projectID = "41c02377-56b0-4b93-b5f4-c13869778ff0"
-           userName = "Daniel"
-           userSecret = "123123"
-           renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} /> }
-           />
-    )
-}
+  if (!localStorage.getItem('username')) return <LoginForm />;
+
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+    />
+  );
+};
+
 
 export default App;
