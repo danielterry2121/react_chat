a7b324d4222c44c1ddce0cc09782f86983a1e5b5
Daniel Terry <daniel.terry2121@gmail.com>
2021-05-15 15:23:10 -0400

    Started up a basic chat application with react's chat engine

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { ChatEngine } from 'react-chat-engine';
+
+import './App.css';
+
+const App = () => {
+    return (
+        <ChatEngine
+           height="100vh"
+           projectID = "41c02377-56b0-4b93-b5f4-c13869778ff0"
+           userName = "Daniel"
+           userSecret = "123123"
+           />
+    )
+}
+
+export default App;
